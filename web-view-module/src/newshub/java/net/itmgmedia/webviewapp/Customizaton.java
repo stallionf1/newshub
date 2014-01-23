0e2086913e8767e0c8f401ce92c618fba5a4bce9
Anton Liaskovskyi <anton.liaskovskyi@gmail.com>
2014-01-23 12:06:08 +0200

    just added a comment. this is a test commit

--- web-view-module/src/newshub/java/net/itmgmedia/webviewapp/Customizaton.java
+++ web-view-module/src/newshub/java/net/itmgmedia/webviewapp/Customizaton.java
@@ -8,4 +8,5 @@ public class Customizaton {
     public static final String WEB_HOST_SUFFIX = WEB_HOST;
     public static final String START_URL = "http://" + WEB_HOST_SUFFIX;
     public static final boolean IS_ALL_HOSTS_ACCEPTED = true;
+    //just a comment: test commit by Anton L
 }
